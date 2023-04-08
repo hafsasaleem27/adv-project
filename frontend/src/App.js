b286c989fbf87c47f7af25dd06cd0f07cbe50329
Hafsa Saleem <hafsa.saleem27@gmail.com>
2023-04-08 18:02:32 +0500

    made links active in the nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,19 +20,26 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import EventsPage from "./pages/EventsPage";
 import EventDetailPage from "./pages/EventDetailPage";
 import NewEventPage from "./pages/NewEventPage";
 import EditEventPage from "./pages/EditEventPage";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage/> },
-  { path: "/events", element: <EventsPage/> },
-  { path: "/events/:eventId", element: <EventDetailPage/> },
-  { path: "/events/new", element: <NewEventPage/> },
-  { path: "/events/:eventId/edit", element: <EditEventPage/> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "", element: <HomePage /> },
+      { path: "events", element: <EventsPage /> },
+      { path: "events/:eventId", element: <EventDetailPage /> },
+      { path: "events/new", element: <NewEventPage /> },
+      { path: "events/:eventId/edit", element: <EditEventPage /> },
+    ],
+  },
 ]);
 
 function App() {
