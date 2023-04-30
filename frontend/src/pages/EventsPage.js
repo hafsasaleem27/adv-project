e7b28a84602319530ff762e06a78b5c274d3be91
Hafsa Saleem <hafsa.saleem27@gmail.com>
2023-04-30 21:23:48 +0500

    bug fix

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -14,7 +14,7 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/eventspadj");
+  const response = await fetch("http://localhost:8080/events");
   // send request to a wrong url to cause error state
 
   if (!response.ok) {
