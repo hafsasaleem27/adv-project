d81bbb2e307b94efb93d06a43b7bf84ae4193cb9
Hafsa Saleem <hafsa.saleem27@gmail.com>
2023-05-13 21:05:21 +0500

    deleted event using useSubmit() hook

--- frontend/src/pages/EventDetailPage.js
+++ frontend/src/pages/EventDetailPage.js
@@ -1,24 +1,36 @@
-import { json, useRouteLoaderData } from "react-router-dom";
+import { json, redirect, useRouteLoaderData } from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-  const data = useRouteLoaderData('event-detail');
+  const data = useRouteLoaderData("event-detail");
 
-  return (
-    <EventItem event={data.event} />
-  );
+  return <EventItem event={data.event} />;
 }
 
 export async function loader({ request, params }) {
   const id = params.eventId;
 
-  const response = await fetch("http://localhost:8080/events/" + id );
+  const response = await fetch("http://localhost:8080/events/" + id);
 
   if (!response.ok) {
-    throw json({ message: 'Could not fetch event!' }, { status: 500 });
+    throw json({ message: "Could not fetch event!" }, { status: 500 });
   } else {
     return response;
   }
+}
+
+export async function action({ request, params }) {
+  const eventId = params.eventId;
+
+  const response = await fetch("http://localhost:8080/events/" + eventId, {
+    method: request.method,
+  });
+
+  if (!response.ok) {
+    throw json({ message: "Could not delete event!" }, { status: 500 });
+  }
+
+  return redirect("/events");
 }
 
 export default EventDetailPage;
